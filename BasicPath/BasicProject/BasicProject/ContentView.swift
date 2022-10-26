7879c8379dbf99003738a3cf68865ff0381ef699
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2022-10-26 02:39:16 -0500

    Cambio desde Xcode

--- BasicPath/BasicProject/BasicProject/ContentView.swift
+++ BasicPath/BasicProject/BasicProject/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("First Project!")
+            Text("Enviar desde XCode")
         }
         .padding()
     }
