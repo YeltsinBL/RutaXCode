a6f41562be8a8b86e2c7f411acecf7f079ae8798
Guillermo Yeltsin Baltodano León <gbl_leo_30@hotmail.com>
2022-10-26 00:25:12 -0500

    First Project Hello World

---
+++ BasicPath/BasicProject/BasicProject/BasicProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicProjectApp.swift
+//  BasicProject
+//
+//  Created by YeltsinMacPro13 on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BasicProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicPath/BasicProject/BasicProject/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  BasicProject
+//
+//  Created by YeltsinMacPro13 on 26/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            Text("First Project!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
